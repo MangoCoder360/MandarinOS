6103d3967873fec2f1e45f96783934cb91b16754
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-07-29 13:00:21 -0700

    Working on macOS V0.1

--- Source Code/Mac Development/main.py
+++ Source Code/Mac Development/main.py
@@ -1,2 +1,6 @@
 from easygui import *
+from time import *
 msgbox("Welcome to Mandarin OS, your system is starting!","System Boot Wizard")
+sleep(0.5)
+msgbox("Loading, please wait...","System Boot Wizard")
+sleep(1)
