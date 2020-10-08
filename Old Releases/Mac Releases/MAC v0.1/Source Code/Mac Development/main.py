06cdaa8d2de0033ae92babdd0e98b26872422231
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-10-08 09:43:41 -0700

    Added files to the archive

---
+++ Old Releases/Mac Releases/MAC v0.1/Source Code/Mac Development/main.py
@@ -0,0 +1,12 @@
+from easygui import *
+from time import *
+
+def desktop():
+	msgbox('Hello there, I am the computer I am here to tell you that this is as far as the program will go for a while,','Desktop')
+	msgbox('My programmers are stumped at how to create a functional desktop with apps and everything, so please note there will be no desktop for quite a while.','Desktop')
+
+msgbox("Welcome to Mandarin OS, your system is starting!","System Boot Wizard")
+sleep(0.5)
+msgbox("Loading, please wait...","System Boot Wizard")
+sleep(1)
+desktop()
---
+++ Old Releases/Windows Releases/WINDOWS v0.6/INSTRUCTIONS.md
@@ -0,0 +1,9 @@
+# Windows Instructions
+1. Download the MandarinOS Windows.zip file
+2. Extract the zip file
+3. Create a folder where you would like Mandarin OS to store its files
+4. Put main.exe and ActivationTool.exe in that folder
+5. Run ActivationTool.exe
+6. Enter your serial number (Contact me to get one)
+6. To start Mandarin OS, run main.exe
+7. Enjoy!
