cbde355bb0cf330cfe982a3a639916dde218ab8f
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-10-17 16:02:34 -0700

    Update OnlineMandarin.py

--- Source Code/Windows Development/OnlineMandarin.py
+++ Source Code/Windows Development/OnlineMandarin.py
@@ -7,8 +7,30 @@ import webbrowser
 client = pymongo.MongoClient("mongodb://MangoCoder360:mangomongo321@cluster0-shard-00-00.vckzv.mongodb.net:27017,cluster0-shard-00-01.vckzv.mongodb.net:27017,cluster0-shard-00-02.vckzv.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-e8h9oi-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client["WindowsApp"]
 coll = db["Users"]
+print("Connected to MongoDB User Servers!")
 loginOption = easygui.buttonbox("Welcome to MandarinOS Online! We are connected to MongoDB and are ready to start!", choices = ["Login to existing account", "Create an account", "Activate an account"])
-if loginOption = "Login to existing account":
+if loginOption == "Login to existing account":
     username = easygui.enterbox("Enter your username:")
     password = easygui.enterbox("Enter your password:")
-    getData = coll.find({"username":username}) #WORK IN PROGRESS
+    getData = coll.find({"username":username}, {"password": 1, "_id": 0})
+    for i in getData: data = i
+    realPassword = data["password"]
+    if realPassword == password:
+        easygui.msgbox('You have been signed in sucsesfully! Please wait while we retrive your user data...')
+    else:
+        easygui.msgbox('We could not find your account :(')
+if loginOption == "Create an account":
+    username = easygui.enterbox("Choose a username:")
+    password = easygui.enterbox("Choose a password:")
+    if easygui.ccbox("Are you sure you would like to create your account?") == True:
+        record2send = {"username":username, "password":password}
+        coll.insert_one(record2send)
+        easygui.msgbox('Your account has been created sucesfully!')
+    else:
+        easygui.msgbox('No accounts have been created')
+if loginOption == "Activate an account":
+    username = easygui.enterbox("Enter your username:")
+    password = easygui.enterbox("Enter your password:")
+    serialKey = easygui.enterbox('Enter your serial number:')
+    #WORK IN PROGRESS
+    #NOT FINISHED
