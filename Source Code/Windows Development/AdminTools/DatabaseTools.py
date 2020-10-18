1cafb3a8fd5f4d452b317939c7c9befaf7239898
Reuben Schiopu <reubenschiopu18@outlook.com>
2020-10-18 09:54:49 -0700

    Added database tools for developers

--- Source Code/Windows Development/AdminTools/DatabaseTools.py
+++ Source Code/Windows Development/AdminTools/DatabaseTools.py
@@ -1,14 +1,18 @@
 import easygui
 import pymongo
-bttnChs = ["Get Password By Username"]
+client = pymongo.MongoClient("mongodb://MangoCoder360:mangomongo321@cluster0-shard-00-00.vckzv.mongodb.net:27017,cluster0-shard-00-01.vckzv.mongodb.net:27017,cluster0-shard-00-02.vckzv.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-e8h9oi-shard-0&authSource=admin&retryWrites=true&w=majority")
+bttnChs = ["Get password by username", "Create serial number"]
 tool = easygui.buttonbox("Choose a tool to use", choices = bttnChs)
-if tool == "Get Password By Username":
+if tool == "Get password by username":
     toFind = easygui.enterbox("Enter a username to search for the corrosponding password")
-    client = pymongo.MongoClient("mongodb://MangoCoder360:mangomongo321@cluster0-shard-00-00.vckzv.mongodb.net:27017,cluster0-shard-00-01.vckzv.mongodb.net:27017,cluster0-shard-00-02.vckzv.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-e8h9oi-shard-0&authSource=admin&retryWrites=true&w=majority")
     db = client["WindowsApp"]
-    collection = db["Users"]
-    data = collection.find({"username":toFind})
-    for i in data:
-        dataFound = i
-    msg = str(dataFound)
+    coll = db["Users"]
+    data = coll.find_one({"username":toFind})["password"]
+    msg = str(data)
     easygui.codebox("Here are the results for your search:", "RESULTS", msg)
+if tool == "Create serial number":
+    toCreate = easygui.enterbox('Enter a 1 word serial number to activate')
+    db = client['WindowsApp']
+    coll = db['SerialNumbers']
+    record = {"serialNumber":toCreate}
+    coll.insert_one(record)
