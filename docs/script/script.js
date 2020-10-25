b1c20558da4116e6511a922842cedb9eb23aa36e
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-10-24 18:44:45 -0700

    Working on login system

---
+++ docs/script/script.js
@@ -0,0 +1,5 @@
+function submitLogin() {
+  var username = document.getElementById("usrname").value;
+  var password = document.getElementById("psword").value;
+  document.getElementById("loginPlaceholder").value = "Please wait while we sign you in...";
+}
